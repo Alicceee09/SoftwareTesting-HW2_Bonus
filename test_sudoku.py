69fee56e62c2f2b85f896c94222397285f144438
alice <alice@alicedeMacBook-Air.local>
2025-11-17 14:45:21 +0800

    Write a failing test: is_valid(row, col, num)

---
+++ test_sudoku.py
@@ -0,0 +1,21 @@
+from sudoku import SudokuBoard, generate
+
+
+def test_is_valid():
+    initial_grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+    board = SudokuBoard(initial_grid)
+
+    assert board.is_valid(0, 2, 4) is True
+    assert board.is_valid(0, 2, 5) is False
+    assert board.is_valid(0, 2, 8) is False
+    assert board.is_valid(1, 1, 9) is False
